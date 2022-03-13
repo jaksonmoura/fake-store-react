981bcd0ee554a7d0cfd5c8f7f04fdfc398731481
Jakson Moura <jrochelly@gmail.com>
2022-03-13 20:18:08 -0300

    Small adjustments; Add products from a category

--- src/API.js
+++ src/API.js
@@ -1,26 +1,28 @@
 export const BASE_URL = "https://fakestoreapi.com";
 
 export const API = {
-	fetchProducts: async (limit = 5, sort = "asc") => {
+	fetchProducts: async (limit = 10, sort = "asc") => {
 		return await fetch(`${BASE_URL}/products?limit=${limit}&sort=${sort}`)
 			.then((res) => res.json())
-			.then((data) => {
-				return data;
-			});
+			.then((data) => data);
 	},
-
 	fetchProduct: async (productId = 0) => {
 		return await fetch(`${BASE_URL}/products/${productId}`)
 			.then((res) => res.json())
 			.then((data) => data);
 	},
-
-	fetchCategories: async () => {
-		return await fetch(`${BASE_URL}/categories`)
+	fetchProductsCategory: async (category = "", limit = 5, sort = "asc") => {
+		return await fetch(
+			`${BASE_URL}/products/category/${category}?limit=${limit}&sort=${sort}`,
+		)
 			.then((res) => res.json())
 			.then((data) => data);
 	},
-
+	fetchCategories: async () => {
+		return await fetch(`${BASE_URL}/products/categories/`)
+			.then((res) => res.json())
+			.then((data) => data);
+	},
 	fetchCategory: async (category = "", limit = 5, sort = "asc") => {
 		return await fetch(
 			`${BASE_URL}/category/${category}?limit=${limit}&sort=${sort}`,
