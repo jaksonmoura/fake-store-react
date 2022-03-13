7dc8ae9d919048de732979da1cf8c771a17fa03e
Jakson Moura <jrochelly@gmail.com>
2022-03-13 20:16:19 -0300

    Add page

---
+++ src/pages/Category/Category.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { API } from "../../API";
+import ProductItem from "../../components/Products/ProductItem";
+
+import * as S from "../../components/Products/Products.style";
+
+const Category = () => {
+	const [products, setProducts] = useState([{}]);
+	let params = useParams();
+	const category = params.category;
+
+	const fetchProductsFromCategory = async (category = "") => {
+		let products = await API.fetchProductsCategory(category);
+		console.log(products);
+		setProducts(products);
+	};
+
+	useEffect(() => {
+		fetchProductsFromCategory(category);
+	}, [category]);
+
+	return (
+		<>
+			<h1>
+				Products in <em>{category}</em> category.
+			</h1>
+			<S.ProductsCollection>
+				{products.map((product) => (
+					<ProductItem key={product.id} {...product} />
+				))}
+			</S.ProductsCollection>
+		</>
+	);
+};
+
+export default Category;
