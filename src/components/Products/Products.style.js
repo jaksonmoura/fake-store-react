21207ebff1716b1f8940e1b360dcff2bcfcab9b1
Jakson Moura <jakson.moura@ati.to.gov.br>
2022-03-24 10:53:26 -0300

    List WIP

--- src/components/Products/Products.style.js
+++ src/components/Products/Products.style.js
@@ -1,16 +1,38 @@
 import styled from "styled-components";
 
-export const ProductsCollection = styled.ul`
+export const ProductsWrapper = styled.div`
 	width: 100%;
+	overflow: hidden;
+	padding: 0 2rem;
+	position: relative;
+`;
+
+export const ButtonScroll = styled.a`
+	width: 50px;
+	height: 50px;
+	background: #fff;
+	border: 1px solid #ccc;
+	border-radius: 50%;
+	position: absolute;
+	display: inline-flex;
+	justify-content: center;
+	align-items: center;
+	text-decoration: none;
+	box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
+	z-index: 5;
+	top: 40px;
+	${(props) => (props.back ? "left: 0" : "right: 0;")}
+`;
+
+export const ProductsCollection = styled.ul`
+	width: fit-content;
 	display: flex;
 	gap: 2rem;
-	flex-wrap: wrap;
 	padding: 0;
 `;
 
 export const Product = styled.li`
-	width: 100%;
-	max-width: 250px;
+	width: 250px;
 	min-height: 350px;
 	list-style-type: none;
 	background: #fff;
--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -5,10 +5,33 @@ import * as S from "./Products.style";
 
 const ProductsList = () => {
 	const [products, setProducts] = useState([{}]);
+	const [translationX, setTranslationX] = useState(0);
+	const productsList = document.querySelector("#products-list");
+	const productsWrapper = document.querySelector("#products-wrapper");
 
 	const fetchProducts = async () => {
 		let productsResult = await API.fetchProducts();
 		setProducts(productsResult);
+	};
+
+	const handleClick = (e, back = false) => {
+		e.preventDefault();
+		let listWidth = productsWrapper.clientWidth;
+		let translationXPositive = Math.abs(translationX);
+		if (back) {
+			let tlX =
+				translationXPositive - listWidth >= 0
+					? translationXPositive - listWidth
+					: 0;
+			setTranslationX(tlX * -1);
+		} else {
+			let tlX =
+				translationXPositive + listWidth >= productsList.clientWidth
+					? translationXPositive - listWidth
+					: 0;
+			setTranslationX(tlX);
+		}
+		console.log(listWidth, productsList.clientWidth, translationXPositive);
 	};
 
 	useEffect(() => {
@@ -16,11 +39,22 @@ const ProductsList = () => {
 	}, []);
 
 	return (
-		<S.ProductsCollection>
-			{products.map((product) => (
-				<ProductItem key={product.id} {...product} />
-			))}
-		</S.ProductsCollection>
+		<S.ProductsWrapper id="products-wrapper">
+			<S.ButtonScroll href="#" back onClick={(e) => handleClick(e)}>
+				<i className="material-icons">keyboard_arrow_left</i>
+			</S.ButtonScroll>
+			<S.ProductsCollection
+				id="products-list"
+				style={{ transform: `translateX(${translationX}px)` }}
+			>
+				{products.map((product) => (
+					<ProductItem key={product.id} {...product} />
+				))}
+			</S.ProductsCollection>
+			<S.ButtonScroll href="#" onClick={(e) => handleClick(e)}>
+				<i className="material-icons">keyboard_arrow_right</i>
+			</S.ButtonScroll>
+		</S.ProductsWrapper>
 	);
 };
 
