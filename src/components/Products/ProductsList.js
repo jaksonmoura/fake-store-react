0ec029dd9dc5eee1519ad69a3cba6dc6f20bf98b
Jakson Moura <jrochelly@gmail.com>
2022-03-28 23:04:15 -0300

    Improve carrousel; Move fetch to page home

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,46 +1,62 @@
-import React, { useState, useEffect } from "react";
-import { API } from "../../API";
+import React, { useState } from "react";
+
 import ProductItem from "./ProductItem";
 import * as S from "./Products.style";
 
-const ProductsList = () => {
-	const [products, setProducts] = useState([{}]);
+const ProductsList = ({ products = [] }) => {
 	const [translationX, setTranslationX] = useState(0);
+	const [arrowDisabled, setArrowDisabled] = useState(false);
 	const productsList = document.querySelector("#products-list");
 	const productsWrapper = document.querySelector("#products-wrapper");
 
-	const fetchProducts = async () => {
-		let productsResult = await API.fetchProducts();
-		setProducts(productsResult);
+	const calculateListTranslate = (back) => {
+		let listVisibleWidth = productsWrapper.clientWidth;
+		let rect = productsList.getBoundingClientRect();
+		if (back) {
+			console.log(translationX + listVisibleWidth);
+			if (translationX + listVisibleWidth > 0) {
+				setTranslationX(0);
+				return;
+			}
+			setTranslationX(translationX + listVisibleWidth);
+		} else {
+			if (listVisibleWidth > rect.right - listVisibleWidth) {
+				if (
+					Math.abs(translationX - listVisibleWidth) >=
+					productsList.clientWidth
+				) {
+					setTranslationX(0);
+					return;
+				}
+				let endofListPosition =
+					listVisibleWidth - (listVisibleWidth - rect.right + 16);
+				setTranslationX(endofListPosition * -1);
+				return;
+			}
+
+			setTranslationX(translationX - listVisibleWidth);
+		}
 	};
 
 	const handleClick = (e, back = false) => {
 		e.preventDefault();
-		let listWidth = productsWrapper.clientWidth;
-		if (back) {
-			if (translationX + listWidth > 0) return;
-			setTranslationX(translationX + listWidth);
-		} else {
-			if (
-				Math.abs(translationX - listWidth) >= productsList.clientWidth
-			) {
-				setTranslationX(0);
-				return;
-			}
-			setTranslationX(translationX - listWidth);
-		}
+		if (arrowDisabled) return;
+		setArrowDisabled(true);
+		calculateListTranslate(back);
+		setTimeout(() => {
+			setArrowDisabled(false);
+		}, 300);
 	};
-
-	useEffect(() => {
-		fetchProducts();
-	}, []);
 
 	return (
 		<S.ProductsWrapper id="products-wrapper">
 			<S.ButtonScroll
 				href="#"
-				style={{ display: translationX < 0 ? "inline-flex" : "none" }}
+				style={{
+					visibility: translationX < 0 ? "visible" : "hidden",
+				}}
 				back
+				disabled={arrowDisabled}
 				onClick={(e) => handleClick(e, true)}
 			>
 				<i className="material-icons">keyboard_arrow_left</i>
@@ -53,7 +69,11 @@ const ProductsList = () => {
 					<ProductItem key={product.id} {...product} />
 				))}
 			</S.ProductsCollection>
-			<S.ButtonScroll href="#" onClick={(e) => handleClick(e)}>
+			<S.ButtonScroll
+				href="#"
+				disabled={arrowDisabled}
+				onClick={(e) => handleClick(e)}
+			>
 				<i className="material-icons">keyboard_arrow_right</i>
 			</S.ButtonScroll>
 		</S.ProductsWrapper>
