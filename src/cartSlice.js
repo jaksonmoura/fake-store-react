db28a1c56b26a482e87625ee30a78e2378dfd1e0
Jakson Moura <jakson.moura@ati.to.gov.br>
2022-04-01 09:19:17 -0300

    FIX: AddToCart updates qty if same item is added

--- src/cartSlice.js
+++ src/cartSlice.js
@@ -5,24 +5,28 @@ const CartSlice = createSlice({
 	initialState: [],
 	reducers: {
 		addToCart: (state, action) => {
-			// TODO: Verify if item exists, if so increase qty
-			console.log(action.id);
-			const updatedItems = state.map((item) => {
-				if (item.id === action.payload.id) {
-					return { ...item, qty: action.payload.qty + 1 };
-				}
-				return item;
-			});
-
-			// return [
-			// 	...state,
-			// 	{ id: action.payload.id, item: { ...action.payload }, qty: 1 },
-			// ];
-			return updatedItems;
+			if (state.length !== 0) {
+				const itemsUpdated = state.map((item) => {
+					if (item.id !== action.payload.id) {
+						return item;
+					}
+					return {
+						...item,
+						qty: item.qty + 1,
+					};
+				});
+				return itemsUpdated;
+			} else {
+				state.push({
+					id: action.payload.id,
+					item: action.payload,
+					qty: state.qty + 1 || 1,
+				});
+			}
 		},
-		// removeFromCart: (state, action) => {
-		//     state.find
-		// }
+		removeFromCart: (state, action) => {
+			state.filter((item) => item.id !== action.payload);
+		},
 	},
 });
 
